2e4c7f8f3c81502a928deda8083dd7a91939257d
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-12 07:59:45 +0000

    conversion

---
+++ 01_basics/03-operationConversion.js
@@ -0,0 +1,52 @@
+//.. to Number ..//
+let numstring=null
+let num=Number(numstring)
+console.log(typeof numstring)
+console.log(typeof num)
+console.log(num)
+
+let numstring2="17aditi"
+let num2=Number(numstring2)
+console.log(typeof numstring2)
+console.log(typeof num2)
+console.log(num2)
+
+// "32" -> 32
+// "17aditi" -> NaN
+// true-> 1; false-> 0;
+// null-> 0;
+
+
+//.. to Boolean ..//
+let bool=9.0
+let isbool=Boolean(bool);
+console.log(typeof bool)
+console.log(typeof isbool)
+console.log(isbool)
+
+// ""-> false
+// "kishika"-> true
+// 0-> false; other num=true;
+
+//.. to String ..//
+let num3=null
+let stringnNum=String(num3)
+console.log(typeof num3)
+console.log(typeof stringnNum)
+console.log(stringnNum)
+
+// *************** Operations **************//
+
+// let value = 2
+// let neg=-value
+// console.log(neg);
+
+// console.log(2+2)
+// console.log(2-7)
+// console.log(9*-3)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(10/3)
+// console.log(4%5)
+
+

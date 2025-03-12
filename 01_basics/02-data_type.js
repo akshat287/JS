885ca11ab88536e69f0b69e4cfe1b73d18ac45a6
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-12 07:24:19 +0000

    data type

---
+++ 01_basics/02-data_type.js
@@ -0,0 +1,22 @@
+"use strict"; // treats all JS code as newer version
+
+// alert("hello")  // throws error, we are using nodejs, not browser
+// alert(3+3)
+
+let name="Akshat"
+let age=20
+let isNoob=true
+
+// number -> 2^52
+// bigint 
+// string -> ""/''
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// symbol -> unique
+//object
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
+
+//console.table(typeof [age,name])

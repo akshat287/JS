64e6cc78b74032ed0aa9bc6938229c2f6b39b21c
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-12 07:05:52 +0000

    basic js

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,21 @@
+const accId = 827
+let accEmail="akshat@gmail.com"
+var accPassword="1108"
+accCity="Indore"
+let accState;
+//accName;
+
+//accId=987
+
+accEmail="kishika@gmail.com"
+accPassword="aditi"
+accCity="Ayodhya"
+
+console.log(accId)
+
+console.table([accId,accEmail,accPassword,accCity,accState])
+
+/*
+    do not use var, it does not works on concept of scope, may lead to change in values or loss of data
+*/
+

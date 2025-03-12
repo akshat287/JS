d066d940b42ce92c719bb10807afbbc52148a866
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-12 08:26:54 +0000

    comparison

---
+++ 01_basics/04-comparison.js
@@ -0,0 +1,28 @@
+//.. simillar data type ..//
+console.log(1<2)
+console.log(2>=1)
+console.log(2==1)
+console.log(2!=1)
+
+//.. js converts the data type to compare ..//
+console.log("2" > 1)
+console.log("02" <8)
+console.log(5=="5")
+
+console.log(null>0)
+console.log(null>=0)
+
+console.log(null==0)
+console.log(null!=0)
+
+console.log(null<0)
+console.log(null<=0)
+
+console.log(undefined ==0)
+console.log(undefined>0)
+console.log(undefined<0)
+
+// === (Strict check, also checks for data types)
+
+console.log("2" === 2)
+console.log("2" == 2)

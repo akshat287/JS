6ba994fa296b2574405fcd2905ba2f3d7ae2ee4d
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-14 19:14:16 +0000

    maths & Number

---
+++ 01_basics/06-maths.js
@@ -0,0 +1,38 @@
+// const score=400
+// console.log(score)
+
+// const balance=new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+const num=38.7338
+//console.log(num.toPrecision(3)) 
+const num1=138.7338
+//console.log(num1.toPrecision(3)) 
+
+const num2=1000000
+//console.log(num2.toLocaleString()); //default standerd
+//console.log(num2.toLocaleString('en-IN')) //...Indian standerd....//
+
+
+// ..................... MATHEmatics ................. //
+
+console.log(Math)
+
+// console.log(Math.abs(-32))
+// console.log(Math.round(8.39))
+// console.log(Math.ceil(4.01))
+// console.log(Math.floor(3.999999))
+// console.log(Math.min(2,4,3,5,1))
+// console.log(Math.max(2,4,3,5,1))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1) // ranges: 1 - 10
+console.log(Math.floor(Math.random()*10)+1)
+
+const min=10
+const max=50
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)

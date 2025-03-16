b2ca05c23c926ea6a7b38c59addb07330f7cd9e0
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-16 09:02:07 +0000

    more on array

---
+++ 02_Array/02-moreOnArray.js
@@ -0,0 +1,28 @@
+const marvel=["ironman","thor","spiderman"]
+const dc=["superman","batman","joker"]
+
+//marvel.push(dc)
+// console.log(marvel)
+// console.log(marvel[3][2])
+
+// const all=marvel.concat(dc)
+// console.log(all)
+
+// const all=[...marvel,...dc]
+// console.log(all)
+
+// const arr=[1,2,3,[4,5,6],7,[8,9,[10]]]
+// console.log(arr)
+// const arr1=arr.flat(Infinity)
+// console.log(arr1)
+
+
+console.log(Array.isArray("KISHIKA"))
+console.log(Array.from("AKSHAT"))
+console.log(Array.from({name: "giveupondreamsanddie"}))
+
+let s1=100
+let s2=200
+let s3=300
+
+console.log(Array.of(s1,s2,s3))

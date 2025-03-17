f48512e7033cdea118963ad16a0c1144130dbf17
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-17 19:02:17 +0000

    hoisting_basic

--- 03_Function/02-scopes.js
+++ 03_Function/02-scopes.js
@@ -8,7 +8,23 @@ if(true){
     //var c=30
 }
 
-console.log(a)
-console.log(b)
-//console.log(c)
+// console.log(a)
+// console.log(b)
+// //console.log(c)
 
+
+
+/******** Hoisting basic **********/
+
+console.log(one(3)) // executes without error
+function one(num){
+    return num+1
+}
+console.log(one(3))
+
+
+console.log(two(3)) // throws erorr: Cannot access 'two' before initialization
+const two=function(num){
+    return num+2
+}
+console.log(two(3))

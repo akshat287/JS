b5903db2e0b42cb9bf4f051988a5c3e099cea031
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-18 08:36:20 +0000

    object_Iteration

---
+++ 05_Iteration/04-object.js
@@ -0,0 +1,15 @@
+const obj={
+    js:'javascript',
+    cpp:'c plus plus',
+    rb:'ruby'
+}
+
+for (const i in obj) {
+    //console.log(`${i} for value ${obj[i]}`)
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}

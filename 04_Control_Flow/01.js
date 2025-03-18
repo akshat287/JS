b15766164e93d2db71c507f66ff8b3b8239ea7af
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-18 07:06:33 +0000

    if else

---
+++ 04_Control_Flow/01.js
@@ -0,0 +1,17 @@
+// if
+const temp=30
+// if(condition){
+//     // code will run if condition is true
+// }
+
+// comparison
+
+// <, >, <=, >=, ==, !=, ===
+
+if(temp>50){
+    console.log("temp is more then 5")
+}
+else{
+    console.log("temp is less than 50")
+}
+console.log("Execute")

45e2b8f055082b44d8d7c6ee1470b401921fc4a1
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-18 07:15:44 +0000

    switchCase

---
+++ 04_Control_Flow/02.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const code=24
+switch (code) {
+    case 24:
+        console.log("akshat")
+        break;
+    case 17:
+        console.log("aditi")
+        break;
+    case 132:
+        console.log("kishika")
+        break;
+    case 32:
+        console.log("amisha")
+        break;
+
+    default:
+        console.log("shruti")
+        break;
+}

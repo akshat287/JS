5fa988217cf4350066efe7a1448f278b9a5895ed
akshat287 <159622012+akshat287@users.noreply.github.com>
2025-03-18 08:13:03 +0000

    while/do_while

---
+++ 05_Iteration/02-while.js
@@ -0,0 +1,23 @@
+
+let i=1;
+while (i<=10) {
+    //console.log(`Value is ${i}`)
+    i+=2
+}
+
+let arr=["batman","joker","robin","beastboy"]
+let j=0
+while(j<arr.length){
+   // console.log(`Superrr: ${arr[j]}`)
+    j++;
+}
+
+let k=11
+do {
+    //console.log(`value is ${k}`)
+} while (k<=10);
+
+do {
+    //console.log(`value is ${k}`)
+    k--
+} while (k>0);
